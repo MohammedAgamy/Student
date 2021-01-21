d1290b16632ed0c73a83ad03337c1be964da0fa7
X ORG <yghnam334455@gmail.com>
2021-01-21 16:47:47 +0200

    Update byGeno PDF finish

--- app/src/main/java/com/com/student/fragment/BookFragment.java
+++ app/src/main/java/com/com/student/fragment/BookFragment.java
@@ -1,25 +1,35 @@
 package com.com.student.fragment;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.com.student.Adapter.PDFAdapter;
+import com.com.student.Listener.ItemClickListener;
+import com.com.student.Model.PDFModel;
 import com.com.student.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BookFragment extends Fragment {
 
-
+    RecyclerView recyclerView;
+    public static List<PDFModel> list;
+    Fragment fragment;
+    PDFAdapter adapter;
 
     public BookFragment() {
     }
-
 
 
     @Override
@@ -32,7 +42,34 @@ public class BookFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.Rv);
 
+        list = new ArrayList<>();
+        list.add(new PDFModel("PDF One", "https://firebasestorage.googleapis.com/v0/b/intro-slider-82f6f.appspot.com/o/uploads%2F1609973936479.pdf?alt=media&token=baab24f4-0009-47f8-92f2-4e951403b46a"));
+        list.add(new PDFModel("PDF Two", "https://firebasestorage.googleapis.com/v0/b/intro-slider-82f6f.appspot.com/o/uploads%2F1609974053307.pdf?alt=media&token=6aaefaf6-716d-44df-a34c-87ff0a376979"));
+        list.add(new PDFModel("PDF Three", "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"));
+        list.add(new PDFModel("PDF Four", "https://drive.google.com/file/d/1lmfLT-oecAsu3L-Uy0_3BttCCyCwYdUX/view?usp=sharing"));
+        list.add(new PDFModel("PDF Five", "http://www.pdf995.com/samples/pdf.pdf"));
+        list.add(new PDFModel("PDF Six", "https://www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf"));
+        list.add(new PDFModel("PDF Seven", "https://www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf"));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        ItemClickListener itemClickListener = new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                fragment = new Pdf_Fragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.add(R.id.fragment_container1, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
 
+                Bundle bundle = new Bundle();
+                bundle.putString("url", list.get(position).getPdfUrl());
+                bundle.putInt("position",position);
+
+                fragment.setArguments(bundle);
+            }
+        };
+        PDFAdapter adapter = new PDFAdapter(list,getContext(),itemClickListener);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/com/student/fragment/Pdf_Fragment.java
+++ app/src/main/java/com/com/student/fragment/Pdf_Fragment.java
@@ -1,17 +1,24 @@
 package com.com.student.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.com.student.R;
 
 
 public class Pdf_Fragment extends Fragment {
+    WebView webView;
+    private ProgressBar progressBar;
 
     public Pdf_Fragment() {
         // Required empty public constructor
@@ -23,5 +30,34 @@ public class Pdf_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pdf_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        webView = view.findViewById(R.id.WV);
+        progressBar = view.findViewById(R.id.pb);
+        progressBar.setVisibility(View.VISIBLE);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        String url =getArguments().getString("url");
+        final   int position =getArguments().getInt("position",0);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:(function() { " +
+                        "document.querySelector('[role=\"toolbar\"]').remove();})()");
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+         webView.loadUrl("https://docs.google.com/gview?embedded=true&url="+ BookFragment.list.get(position).getPdfUrl());
+      //  webView.loadUrl("https://docs.google.com/viewerng/viewer?embedded=true&url=" + url);
+      //  webView.loadUrl("https://docs.google.com/viewerng/viewer?embedded=true&url=");
+      //  webView.loadUrl(url);
     }
 }
